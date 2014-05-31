6eb3be486f89fcdbbf3c3ce7685194424476dd2b
Gilles LOURDELET <glourdel@student.42.fr>
2014-05-31 16:09:48 +0200

    une planete et une fee sans ailes

---
+++ src/visu_src/models/Engine.cpp
@@ -0,0 +1,84 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   Engine.cpp                                         :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: glourdel <glourdel@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2014/05/31 14:10:28 by glourdel          #+#    #+#             //
+//   Updated: 2014/05/31 16:06:53 by glourdel         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Engine.h"
+
+Engine::Engine()
+{
+	SKeyMap	keyMap[5];
+
+	m_device = createDevice(video::EDT_OPENGL,
+							core::dimension2d<u32>(2560, 1440),
+							32, true, true, false, 0);
+	m_device->getCursorControl()->setVisible(false);
+#ifdef _IRR_OSX_PLATFORM_
+	m_device->getFileSystem()->changeWorkingDirectoryTo ("visu.app");
+	m_device->getFileSystem()->changeWorkingDirectoryTo ("Contents");
+	m_device->getFileSystem()->changeWorkingDirectoryTo ("Resources");
+#endif
+	m_driver = m_device->getVideoDriver();
+	m_sceneManager = m_device->getSceneManager();
+	keyMap[0].Action = EKA_MOVE_FORWARD;  // avancer
+	keyMap[0].KeyCode = KEY_KEY_W;        // w
+	keyMap[1].Action = EKA_MOVE_BACKWARD; // reculer
+	keyMap[1].KeyCode = KEY_KEY_S;        // s
+	keyMap[2].Action = EKA_STRAFE_LEFT;   // a gauche
+	keyMap[2].KeyCode = KEY_KEY_A;        // a
+	keyMap[3].Action = EKA_STRAFE_RIGHT;  // a droite
+	keyMap[3].KeyCode = KEY_KEY_D;        // d
+	keyMap[4].Action = EKA_JUMP_UP;       // saut
+	keyMap[4].KeyCode = KEY_SPACE;        // barre espace
+	m_sceneManager->addCameraSceneNodeFPS(0, 100.0f, 0.1f, -1, keyMap, 5);
+	m_trentorMesh = m_sceneManager->getMesh("models/Faerie2.x");
+	m_planetMesh = m_sceneManager->getMesh("models/earth.x");
+	m_trentorSkin1 = m_driver->getTexture("models/Faerie2D_Skin.jpg");
+	m_trentorWing1 = m_driver->getTexture("models/Faerie2D_Wing.PNG");
+	m_planetTexture = m_driver->getTexture("models/earth.jpg");
+}
+
+Engine::~Engine()
+{
+	m_device->drop();
+}
+
+void	Engine::addPlanet()
+{
+	m_planet = m_sceneManager->addAnimatedMeshSceneNode(m_planetMesh);
+	m_planet->setScale(core::vector3df(2.0f));
+	m_planet->setMaterialFlag(video::EMF_LIGHTING, false);
+	m_planet->setMaterialFlag(video::EMF_ANTI_ALIASING, true);
+	m_planet->setMaterialFlag(video::EMF_ANISOTROPIC_FILTER, true);
+	m_planet->setMaterialTexture(0, m_planetTexture );
+}
+
+void	Engine::addTrantors()
+{
+	m_trentor1 = m_sceneManager->addAnimatedMeshSceneNode(m_trentorMesh);
+//	m_trentor1->setMD2Animation(scene::EMAT_STAND);
+	m_trentor1->setMaterialFlag(video::EMF_LIGHTING, false);
+	m_trentor1->setMaterialFlag(video::EMF_ANTI_ALIASING, true);
+	m_trentor1->setMaterialFlag(video::EMF_ANISOTROPIC_FILTER, true);
+	m_trentor1->setMaterialTexture(0, m_trentorSkin1 );
+	m_trentor1->setMaterialTexture(1, m_trentorWing1 );
+	m_trentor1->setMaterialType(video::EMT_TRANSPARENT_ALPHA_CHANNEL_REF);
+}
+
+void	Engine::loop()
+{
+	while(m_device->run())
+	{
+		m_driver->beginScene (true, true,
+							  video::SColor(255, 0, 0, 0));
+		m_sceneManager->drawAll();
+		m_driver->endScene();
+	}
+}
---
+++ visu.app/Contents/Resources/models/Faerie_ReadMe.txt
@@ -0,0 +1,77 @@
+Faerie lowpoly model
+Copyright 2012, BirdaoGwra
+e-mail: birdaogwra@gmail.com
+
+
+
+INSTALLATION INSTRUCTIONS:
+
+To install, simply unzip to your hard drive and you're ready to go!
+
+
+
+USAGE INFORMATION:
+
+This zip contains the models, textures and animation info for that particular format!
+
+MODEL INFO:
+
+Tatal polycount 904
+
+
+ANIMATION INFO:
+
+Direct X model format:
+Idle = 1 to 39
+Run = 40 to 45
+Attack 46 to 53
+Jump  = 54 to 68
+Suspect = 69 to 74
+Salute ( Flying kiss ) = 75 to 86
+Jump Spine = 87 to 106
+Bye = 107 to 120
+Power Attack = 121 to 128
+Pain1 = 129 to 133
+Pain2 = 133 to 138
+Pain3 = 139 to 146
+Duck Idle = 147 to 175
+Duck Walk = 176 to 182
+Duck Attack = 183 to 189
+Die1 ( Duck ) = 190 to 198
+Die2 = 201 to 205
+Die3 = 207 to 211
+Die4 = 213 to 219
+
+
+Default framerate is 15 fps
+
+
+CREDITS:
+
+Credits to me "BirdaoGwra" are really appreciated but not essential ;-)
+
+Any questions, screenshots of her in use etc, email me at:-
+
+email: birdaogwra@gmail.com
+
+
+WHAT'S INCLUDED IN THE ZIP:
+
+ReadMe.txt - This file
+Faerie.x - DirectX model format
+
+2 skin texture 1024x1024 ( Sweta and Syama )
+2 wing texture 512x512 ( with alpha channel )
+
+*Feel free to draw and use your own texture or change the wing with your own wing texture.
+
+
+RESTRICTIONS:
+
+This model pack is available for use in freeware, shareware, commercial games/software with the following restrictions:-
+
+**You may not sell/re-sell this model pack or claim it as your own.
+***You may not redistribute this pack in  some other model pack through a website or on a compilation CD of any kind, without my written consent.
+
+Enjoy :),
+BirdaoGwra.
