d1c84387403fbeb0c06c5cba291ee19a6c10e938
Tuan ARDOUIN <tuardoui@e2r2p13.42.fr>
2014-06-26 15:33:57 +0200

    IA de test (de merde), en attendant de fix erreur de compil et broadcast fonctionel

--- src/client_src/main.cpp
+++ src/client_src/main.cpp
@@ -6,7 +6,7 @@
 //   By: glourdel <glourdel@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2014/05/28 11:59:19 by glourdel          #+#    #+#             //
-/*   Updated: 2014/06/26 13:23:18 by dcouly           ###   ########.fr       */
+/*   Updated: 2014/06/26 14:41:53 by tuardoui         ###   ########.fr       */
 //                                                                            //
 // ************************************************************************** //
 
@@ -37,17 +37,29 @@ void		co_cmd(int sock, char *buf, char *cmd)
 	printf("BUF : %s\n", buf);
 }
 
-void		*cl_calcul(char *buf, int sock)
+typedef	struct		s_ia
 {
-	int		c;
+			int		eggs;
+}					t_ia;
+
+
+
+void		*cl_calcul(char *buf, int sock, char const *tab[], int i)
+{
+//	int		c;
 	char	*cmd;
 
-	c = rand() % 2;
-	c = 2;
+	sleep(1);
+	printf("\nprintf : %s\n", tab[i]);
+	cmd = strdup(tab[i]);
+	printf("cmd = %s", cmd);
+
+	/*c = rand() % 2;
+	c = 9;
 	switch (c)
 	{
 	case (0) :
-		cmd = strdup("avance\n");
+		cmd = strdup(""avance"\n");
 		break ;
 	case (1) :
 		cmd = strdup("voir\n");
@@ -62,10 +74,10 @@ void		*cl_calcul(char *buf, int sock)
 		cmd = strdup("inventaire\n");
 		break ;
 	case (5) :
-		cmd = strdup("prend linemate\n");
+		cmd = strdup(""prend linemate"\n");
 		break ;
 	case (6) :
-		cmd = strdup("pose linemate\n");
+		cmd = strdup(""pose linemate"\n");
 		break ;
 	case (7) :
 		cmd = strdup("expulse\n");
@@ -74,7 +86,7 @@ void		*cl_calcul(char *buf, int sock)
 		cmd = strdup("broadcaster TOTO\n");
 		break ;
 	case (9) :
-		cmd = strdup("incantation\n");
+		cmd = strdup(""incantation"\n");
 		break ;
 	case (10) :
 		cmd = strdup("fork\n");
@@ -83,10 +95,12 @@ void		*cl_calcul(char *buf, int sock)
 		cmd = strdup("connect_nbr\n");
 		break ;
 	}
+	*/
 	co_cmd(sock, buf, cmd);
 	free(cmd);
 	return (buf);
-}
+
+	}
 
 int			main(int argc, char **argv)
 {
@@ -96,16 +110,22 @@ int			main(int argc, char **argv)
 	char				*addr;
 	char				*port;
 	int					nb;
-
+	char const			*tab_cmd[] = {"avance\n", "prend linemate\n", "droite\n", "droite\n", "avance\n", "pose linemate\n", "droite\n", "droite\n", "avance\n", "broadcast hello\n", "prend deraumere\n", "prend nourriture\n", "droite\n"
+									 , "droite\n", "avance\n", "pose deraumere\n", "droite\n", "droite\n", "avance\n", "prend sibur\n", "droite\n", "droite\n", "avance\n", 
+									 "pose sibur\n", "broadcast hello\n", "droite\n", "droite\n", "avance\n", "prend mendiane\n", "prend nourriture\n", "droite\n", 
+									 "droite\n", "avance\n", "pose mendiane\n", "droite\n", "droite\n", "avance\n", "prend phiras\n", "droite\n", "droite\n", "avance\n", 
+									 "pose phiras\n", "droite\n", "droite\n", "broadcast hello\n", "avance\n", "prend nourriture\n", "prend nourriture\n", "droite\n", 
+									 "droite\n", "avance\n", "pose thystame\n", "incantation\n"};
+	int					i;
+	
 	nb = 0;
+	i = 0;
 	buf = (char*)malloc(1000);
 	if (argc < 5 || argc > 7 || !cl_parse_line(argc, argv, &addr, &port, &team))
 		return (cl_err_usage());
 	cout << "client here ! " << endl;
 	team = strcat(team, "\n");
 	sock = cl_new_connection(addr, port, team);
-
-	recv(sock, buf, 1023, 0);
 /*	if (fork())
 	{
 		sleep(3);
@@ -115,8 +135,9 @@ int			main(int argc, char **argv)
 	{
 		nb++;
 		cout << nb <<endl;
-		cl_calcul(buf, sock);
+		cl_calcul(buf, sock, tab_cmd, i);
 		cout << "end" << endl;
-	} while (strcmp(buf, "mort\n"));
+		i++;
+	} while (strcmp(buf, "mort\n") && i <= 54);
 	return (0);
 }
