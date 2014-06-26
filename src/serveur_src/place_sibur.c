192c540e196b3db3444509a93bf15fd0cf8ae5f7
Baptiste JACOB <bjacob@e2r12p14.42.fr>
2014-06-26 14:10:35 +0200

    repair place sibur

--- src/serveur_src/place_sibur.c
+++ src/serveur_src/place_sibur.c
@@ -6,23 +6,32 @@
 /*   By: bjacob <bjacob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/25 16:54:15 by bjacob            #+#    #+#             */
-/*   Updated: 2014/06/25 16:54:35 by bjacob           ###   ########.fr       */
+/*   Updated: 2014/06/26 14:10:08 by bjacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "map.h"
+#include "types.h"
 #include <stdlib.h>
 #include <time.h>
 
-void		ft_place_sibur(t_area ***map, int x, int y, int nb)
+void		ft_place_sibur(t_data *game, int x, int y, int nb)
 {
 	int		i;
+	int		x_bis;
+	int		y_bis;
 
 	i = 0;
 	srand(time(0));
 	while (i < nb)
 	{
-		map[rand() % x][rand() % y]->sibur++;
+		x_bis = rand() % x;
+		y_bis = rand() % y;
+		game->map[x_bis][y_bis]->sibur++;
+		ft_strcat(game->visu.cmd_out, "ipo 3 ");
+		ft_strcat(game->visu.cmd_out, ft_itoa(x_bis));
+		ft_strcat(game->visu.cmd_out, " ");
+		ft_strcat(game->visu.cmd_out, ft_itoa(y_bis));
+		ft_strcat(game->visu.cmd_out, "\n");
 		i++;
 	}
 }
